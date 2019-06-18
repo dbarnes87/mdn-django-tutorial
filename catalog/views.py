bf2ddb2e2167da609ecc4bb2c14a36695d348762
David Barnes <david.barnes115@gmail.com>
2019-06-18 18:13:16 -0400

    complete tutorial through chapter 6

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
 
 # Create your views here.
+from catalog.models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # The 'all()' is implied by default.    
+    num_authors = Author.objects.count()
+    
+    # books_containing_genre = Book.objects.filter(genre='Fantasy')
+
+    # potter_books = Book.objects.filter(title_contains='potter')
+    # num_potter_books = potter_books.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        # 'books_containing_genre': books_containing_genre,
+        # 'num_potter_books': num_potter_books,
+    }
+
+    
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
